3a6daadb08dddafd19d77b0ae15d0bd954d7ad40
zhiyuan <1457321461@qq.com>
2017-11-01 00:02:53 +0800

    图像处理主界面

---
+++ Src/ImageProcessApp/Header/ImageProcessApp.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+
+
+#include "..\..\..\DuiLib\UIlib.h"
+using namespace DuiLib;
+class CImageProcessApp : public WindowImplBase
+{
+protected:
+	virtual void InitWindow();
+	virtual void Notify(TNotifyUI& msg);
+	virtual void OnClick(TNotifyUI& msg);
+	virtual CDuiString GetSkinFolder();
+	virtual CDuiString GetSkinFile();
+	virtual LPCTSTR GetWindowClassName(void) const;
+	HRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+
+
+
+};
